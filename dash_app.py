350aa67b6722639fd88d85f504135cc055b4f04b
Angelo Salton <gsalton4@hotmail.com>
2020-01-08 14:55:35 -0200

    Enable plots

--- dash_app.py
+++ dash_app.py
@@ -14,9 +14,6 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-# plot styles
-pl_styles = {}
-
 # navbar
 navbar = dbc.NavbarSimple(
     children=[
@@ -60,8 +57,7 @@ body = dbc.Container([
                 dcc.Markdown('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?')
             ]),
             dbc.Col([
-                # dcc.Markdown('grafico aqui')
-                dcc.Graph(figure=plots.gr_ipca(), style=pl_styles)
+                dcc.Graph(figure=plots.gr_ipca())
             ])
             ]),
     # Dados de comércio
@@ -71,8 +67,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_pmc(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_pmc())
         ]),
     ]),
     # Dados de serviços
@@ -82,8 +77,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_pms(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_pms())
         ]),
     ]),
     # Preços ao produtor
@@ -93,8 +87,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_pimpf(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_pimpf())
         ])
     ]),
 
@@ -106,8 +99,7 @@ body = dbc.Container([
                 dcc.Markdown('Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?')
             ]),
             dbc.Col([
-                dcc.Markdown('grafico aqui')
-                # dcc.Graph(figure=plots.gr_focus(), style=pl_styles)
+                dcc.Graph(figure=plots.gr_focus())
             ])
             ]),
     # Dólar PTAX
@@ -117,8 +109,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_ptax(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_ptax())
         ]),
     ]),
     # Dados de commodities ------------------------------------------
@@ -130,8 +121,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # # dcc.Graph(figure=plots.gr_comb_intl(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_comb_intl())
         ]),
     ]),
     # Grãos
@@ -141,8 +131,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_graos(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_graos())
         ]),
     ]),
     # Animais
@@ -152,8 +141,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_animais(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_animais())
         ]),
     ]),
     # Metais
@@ -163,8 +151,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_metais(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_metais())
         ]),
     ]),
     # Gás natural
@@ -174,8 +161,7 @@ body = dbc.Container([
             dcc.Markdown('texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui texto aqui'),
         ]),
         dbc.Col([
-            dcc.Markdown('grafico aqui')
-            # dcc.Graph(figure=plots.gr_gasnat(), style=pl_styles)
+            dcc.Graph(figure=plots.gr_gasnat())
         ])
     ]),
     html.Footer([
