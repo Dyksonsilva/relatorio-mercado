d1b60485c5b24027e9b1e5cc5ad20939e6fa966d
Angelo Salton <gsalton4@hotmail.com>
2020-02-21 13:52:02 -0300

    Test inflation text

--- texts.py
+++ texts.py
@@ -5,7 +5,7 @@
 
 # In plotly dash, Markdown syntax is available.
 
-ipca = 'Dados sobre a *inflação*.'
+ipca = 'A expectativa para o IPCA é de que a aceleração de janeiro para fevereiro seja a menor desde 1994, chegando a 0,22%. As quedas foram puxadas pelo recuo das carnes (-5%) e passagens aéreas (-6,6%). A energia elétrica também contribuiu, com a definição da bandeira tarifária verde em janeiro. A meta para o governo em 2020 é de 4% a.a. para o índice geral, com desvio de 1,5%. Em 2019 a meta era de 4,25% a.a.'
 
 pmc = 'Dados sobre a pesquisa mensal de comércio.'
 
